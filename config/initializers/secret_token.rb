8ddaa77e25947acb2590524f6f9f492a1c78daae
Francis Gulotta <wizard@roborooter.com>
2014-03-19 15:44:19 -0400

    add default secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,4 +7,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-CountingGame::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+CountingGame::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || "a4a47e60da661842bd754c62da62d9a3874a9f3e40a470d5ac930033972fa97e127d29e6b4f38e22c05ecffc6142a5f9544bdceb2bede6740f04eacb95c66a48"
